f913dcfcfa05cda6a45a54585635d4d59b11ae13
Corey <coreydblevins@gmail.com>
2023-06-18 14:53:59 -0400

    more consistant average component

--- src/components/average.js
+++ src/components/average.js
@@ -1,37 +1,40 @@
 
-export const Average = ({average}) => {
+export const Average = ({ average }) => {
     const score = Math.floor(average)
 
-
-  function getColor () {
-    let color = 'rose'
-    switch(true){
-        case score >= 8:
-            color = 'green'
-            break
-        case score < 8 && score >= 5:
-            color = 'yellow'
-            break
-        case score === 0:
-            color = 'slate'
-            break
-        default:
-            break
-          }
-    return color
-  }
-  function getValue () {
-    if (score === 0){
-        return <p>N/A</p>
+    function getColoredScore() {
+        console.log(score)
+        if (score >= 8) {
+            return (
+                <div className={`border border-green-500 text-green-500 rounded p-1 ml-2`}>
+                    <p>{String(average).slice(0, 3)}</p>
+                </div>
+            )
+        }
+        else if (score < 8 && score >= 5) {
+            return (
+                <div className={`border border-yellow-500 text-yellow-500 rounded p-1 ml-2`}>
+                    <p>{String(average).slice(0, 3)}</p>
+                </div>
+            )
+        }
+        else if (score === 0) {
+            return (
+                <div className={`border border-slate-500 text-slate-500 rounded p-1 ml-2`}>
+                    <p>N/A</p>
+                </div>
+            )
+        }
+        else return (
+            <div className={`border border-red-500 text-red-500 rounded p-1 ml-2`}>
+                <p>{String(average).slice(0, 3)}</p>
+            </div>
+        )
     }
-    else{
-        return <p>{String(average).slice(0, 3)}</p>
-    }
-  }
 
     return (
-        <div className={`border border-${getColor()}-500 text-${getColor()}-500 rounded p-1 ml-2`}>
-            <div>{getValue()}</div>
+        <div>
+            {getColoredScore()}
         </div>
     )
 }
